e44bbb28c91acb2451b01aa469a1c92dcacfdcdd
Cyclip <40525455+Cyclip@users.noreply.github.com>
2021-06-14 13:20:56 +0100

    Lib test

---
+++ thing/target/debug/deps/thing-3e8a9f4d1a1cf9a6.d
@@ -0,0 +1,7 @@
+C:\Users\Cyclip-06b\Desktop\Projects\rust_test\thing\target\debug\deps\thing-3e8a9f4d1a1cf9a6.rmeta: src\lib.rs
+
+C:\Users\Cyclip-06b\Desktop\Projects\rust_test\thing\target\debug\deps\libthing-3e8a9f4d1a1cf9a6.rlib: src\lib.rs
+
+C:\Users\Cyclip-06b\Desktop\Projects\rust_test\thing\target\debug\deps\thing-3e8a9f4d1a1cf9a6.d: src\lib.rs
+
+src\lib.rs:
---
+++ thing/target/debug/libthing.d
@@ -0,0 +1,1 @@
+C:\Users\Cyclip-06b\Desktop\Projects\rust_test\thing\target\debug\libthing.rlib: C:\Users\Cyclip-06b\Desktop\Projects\rust_test\thing\src\lib.rs
